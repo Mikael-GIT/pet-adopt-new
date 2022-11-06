51f4f3e60b516a5b496e7f90ab0e014093106132
lokih1 <lokihya@gmail.com>
2022-11-06 02:45:31 -0300

    pagina formulario e validaçao

--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { CheckoutPageRoutingModule } from './checkout-routing.module';
@@ -10,8 +10,9 @@ import { CheckoutPage } from './checkout.page';
 
 @NgModule({
   imports: [
-    CommonModule,
     FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
     IonicModule,
     CheckoutPageRoutingModule
   ],
--- src/app/checkout/checkout.page.ts
+++ src/app/checkout/checkout.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.page.html',
   styleUrls: ['./checkout.page.scss'],
 })
-export class CheckoutPage implements OnInit {
 
-  constructor() { }
+export class CheckoutPage {
 
-  ngOnInit() {
+  get nome() {
+    return this.registrationForm.get('nome');
   }
 
+  public errorMessages = {
+    nome: [
+        {type: 'required', message: 'Digite seu nome'},
+        {type: 'maxlength', message: 'Nome não pode passar de 50 Caracteres'}
+    ]
+  };
+
+  registrationForm = this.formBuilder.group ({
+    nome: ['',[Validators.required, Validators.maxLength(5)]],
+
+  });
+
+
+  constructor(private formBuilder: FormBuilder) {}
+  public submit() {
+    console.log(this.registrationForm.value);
+}
 }
